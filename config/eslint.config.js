0b2342d73c68e7c3ea1555bc7a84f95c4893ca0e
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2019-02-25 17:23:44 +0200

    [Update] eslint setup

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,38 +1,39 @@
 // https://github.com/yannickcr/eslint-plugin-react#configuration
 
 module.exports = {
-  // "extends": "airbnb"
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
+  parser: 'babel-eslint',
+  // extends: "airbnb",
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
   ],
 
-  "settings": {
-    "react": {
-      "createClass": "createReactClass", // Regex for Component Factory to use,
-                                         // default to "createReactClass"
-      "pragma": "React",  // Pragma to use, default to "React"
-      "version": "detect", // React version. "detect" automatically picks the version you have installed.
-                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-      "flowVersion": "0.53" // Flow version
+  settings: {
+    react: {
+      createClass: 'createReactClass', // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: 'React', // Pragma to use, default to "React"
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      flowVersion: '0.53', // Flow version
     },
-    "propWrapperFunctions": [
-        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-        "forbidExtraProps",
-        {"property": "freeze", "object": "Object"},
-        {"property": "myFavoriteWrapper"}
+    propWrapperFunctions: [
+      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+      'forbidExtraProps',
+      { property: 'freeze', object: 'Object' },
+      { property: 'myFavoriteWrapper' },
     ],
-    "linkComponents": [
+    linkComponents: [
       // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-      "Hyperlink",
-      {"name": "Link", "linkAttribute": "to"}
-    ]
+      'Hyperlink',
+      { name: 'Link', linkAttribute: 'to' },
+    ],
   },
 
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    }
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 
 };
