cb428d44a5624e3d56d36e9be00247f9ab4da416
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2019-02-25 17:07:42 +0200

    [Clean]

--- scripts/transpile.js
+++ scripts/transpile.js
@@ -5,12 +5,8 @@ process.env.NODE_ENV = 'production';
 
 const npx = require('../utils/npx');
 const { tools } = require('../utils/resolver');
-const configFilePath = tools.resolve('config/babel.config')
 const babelPreset = tools.resolve('utils/babel-preset-tools')
 
-// babel src -d dist  --source-maps inline --copy-files
-// npx babel src -d dist  --source-maps inline --copy-files --config-file ./config/babel.config.js
-// https://github.com/babel/babel/issues/8919
 module.exports = function({ args }={ args:[] }){
   const app = tools.resolve('node_modules/.bin/babel')
   return npx([
