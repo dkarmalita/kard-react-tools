6738aba2b1a3f856c2282a8692eacb97841c8a4a
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2019-02-27 23:40:57 +0200

    [Fix] babel-ject require

--- utils/babelTransform.js
+++ utils/babelTransform.js
@@ -1,6 +1,6 @@
 const { tools } = require('../utils/resolver');
 
-const babelJest = tools.require('babel-jest');
+const babelJest = tools.require('node_modules', 'babel-jest');
 
 module.exports = babelJest.createTransformer({
   presets: [tools.resolve('utils/babel-preset-tools')],
