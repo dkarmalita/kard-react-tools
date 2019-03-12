4d8e06dc9f91582a4cd7ae490606ca45e24e3096
Dmitriy Karmalita <dmitriy.karmalita@gmail.com>
2019-03-12 23:03:42 +0200

    [++] init

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,11 @@
 const path = require('path')
 const shell = require('shelljs');
-const log = require('../utils/logger');
-const crossSpawn = require('../utils/crossSpawn');
+
+const { tools, target } = require('../utils/resolver');
+
+const log = tools.require('utils/logger');
+const crossSpawn = tools.require('utils/crossSpawn');
+
 console.log(crossSpawn)
 // shell.ln('-s', `${process.cwd()}/dist/apps`, `${process.cwd()}/dist/themes/gmotrading/apps`)
 
