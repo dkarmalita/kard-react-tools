09ed6c338088aa42d818d5eaf10358b3145f6604
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2019-03-18 18:41:18 +0200

    [Advance] init

--- scripts/init.js
+++ scripts/init.js
@@ -1,16 +1,26 @@
-const path = require('path')
-const shell = require('shelljs');
+const path = require('path');
 
 const { tools, target } = require('../utils/resolver');
 
 const log = tools.require('utils/logger');
-const crossSpawn = tools.require('utils/crossSpawn');
 
-console.log(crossSpawn)
-// shell.ln('-s', `${process.cwd()}/dist/apps`, `${process.cwd()}/dist/themes/gmotrading/apps`)
+const shell = tools.require('shelljs');
 
-module.exports = function main(args){
-  log.info('script got the args:', args)
-  log.info('source path',path.join(process.env.PWD, 'templates/default'))
-  log.info('destination path',process.env.PWD)
-}
+module.exports = function main({ args }) {
+  log.info('script got the args:', args);
+  log.info('source path', path.join(process.env.PWD, 'templates/default'));
+  log.info('destination path', process.env.PWD);
+
+  const targetFolderName = args[0];
+
+  if (!targetFolderName) {
+    log.error('projsct folder is not defined');
+    log.help('usage example:');
+    log.help('  react-tools init projectFolder [template]');
+    process.exit(0);
+  }
+  const src = tools.fullPath('templates/default');
+  const dst = target.fullPath(targetFolderName);
+
+  shell.cp('-R', `${src}/`, dst);
+};
--- utils/logger.js
+++ utils/logger.js
@@ -10,14 +10,16 @@ module.exports = {
     return {
       reset: '\x1b[0m',
       fgBlue: '\x1b[34m',
+      fgGreen: '\x1b[32m',
       fgRed: '\x1b[31m',
       fgYellow: '\x1b[33m',
     };
   },
   get _logid() { return packageJson.logid || packageJson.name || path.basename(process.argv[1]); },
-  _text(...args) { console.log(...args); }, // eslint-disable-line no-console
   _log(id, ...args) { this._text(`${this._color.fgBlue}${this._logid}${this._color.reset} [${moment().format('HH:MM:ss.SSS')} ${id}]`, ...args); },
+  _text(...args) { console.log(...args); }, // eslint-disable-line no-console
   error(...args) { this._log(`${this._color.fgRed}ERR${this._color.reset}`, ...args); },
+  help(...args) { this._log(`${this._color.fgGreen}HELP${this._color.reset}`, ...args); },
   info(...args) { this._log(`${this._color.fgBlue}INF${this._color.reset}`, ...args); },
   warn(...args) { this._log(`${this._color.fgYellow}WARN${this._color.reset}`, ...args); },
 };
